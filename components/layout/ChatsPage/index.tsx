7faebea3d0f664cb0be95b9b1db5e2e9e0ed0f82
Faris-Nur <farishakim847@gmail.com>
2024-11-12 22:25:22 +0700

    fix bug scroll-5

--- components/layout/ChatsPage/index.tsx
+++ components/layout/ChatsPage/index.tsx
@@ -214,7 +214,7 @@ const ChatsPage = ({ user }: { user: User | null | undefined }) => {
                     behavior: 'smooth',
                     block: 'center'
                 });
-            }, 10)
+            }, 600)
 
             element.classList.add('bg-blue-200');
             element.classList.add('dark:bg-slate-800');
