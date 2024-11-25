f8585133d10da1cb335b30cde165e6bff9529051
Faris-Nur <farishakim847@gmail.com>
2024-11-26 00:51:09 +0700

    update metadata

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '',
     },
-    sitemap: 'https://tb-xiirpl1-24.vercel.app/sitemap.xml',
+    sitemap: 'https://smktarunabangsa.rpl1.xyz/sitemap.xml',
   }
 }
